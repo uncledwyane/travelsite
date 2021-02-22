524f1cbdf482588259def91fcb09584638888b2e
wade <327656879@qq.com>
2021-02-22 21:02:33 +0800

    j

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = new express();
-const cors = require('cors');
+const cors = require('express-cors');
 const mysqls = require('./mysqls');
 const chalk = require('chalk');
 // 导入状态码
@@ -15,17 +15,25 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(cors())
+app.use(cors({
+    allowedOrigins: [
+        'github.com', 'google.com', 'http://localhost:3000'
+    ]
+}))
 
-// 设置为可跨域
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-});
+//设置跨域访问
+app.all("*",function(req,res,next){
+    //设置允许跨域的域名，*代表允许任意域名跨域
+    res.header("Access-Control-Allow-Origin","*");
+    //允许的header类型
+    res.header("Access-Control-Allow-Headers","content-type");
+    //跨域允许的请求方式 
+    res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+    if (req.method.toLowerCase() == 'options')
+        res.send(200);  //让options尝试请求快速结束
+    else
+        next();
+})
 
 
 app.get('/alluser', function(req, res){
